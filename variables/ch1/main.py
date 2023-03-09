d812148cb024870b6ff316e0fa5ef447fa4276c2
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-09 23:10:41 +0300

    while insert character 'y' then continue

--- variables/ch1/main.py
+++ variables/ch1/main.py
@@ -1,9 +1,12 @@
-num=input('Insert an integer: ')
-# Check input value 
-if num.isdigit():
-    num=int(num)
-    print('Double ',num,'is: ',2*num)
-else:
-    print(' ',num,'is not integer: ')
-# Accept Only integer number 
+continues = 'y'
+while continues=='y':
+    continues = input('Do you continue?(y/n)')
+    num=input('Insert an integer: ')
+    # Check input value 
+    if num.isdigit():
+        num=int(num)
+        print('Double ',num,'is: ',2*num)
+    else:
+        print(' ',num,'is not integer: ')
+    # Accept Only integer number 
 

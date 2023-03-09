4c95f7a80f4d9cdecfc3d2af6387fb102c1890dc
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-09 19:33:40 +0300

    insert comments

--- variables/ch1/main.py
+++ variables/ch1/main.py
@@ -1,7 +1,9 @@
 num=input('Insert an integer: ')
+# Check input value 
 if num.isdigit():
     num=int(num)
     print('Double ',num,'is: ',2*num)
 else:
     print(' ',num,'is not integer: ')
-    
+# Accept Only integer number 
+

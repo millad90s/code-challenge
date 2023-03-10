73f32288ce21ef89504f49221f3622e791182d9a
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-10 21:34:12 +0300

    Define chars for pass

---
+++ variables/Ch4/Random-Password-Generate.py
@@ -0,0 +1,23 @@
+'''Password Generator: Create a program that generates a random password for the user using variables to store the password.'''
+import random
+# Define valid char
+symbol = "!@#$%^&*()_+"
+number = "1234567890"
+letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# Define valid Number char = 8(1+2+5)
+num_symbol = 1
+num_number = 2
+num_letter = 5
+
+
+
+
+
+
+
+
+
+
+
+

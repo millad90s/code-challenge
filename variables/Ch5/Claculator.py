0c1f191b6fdbdd982ac3f5828b4b30e4cd002feb
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-11 00:01:27 +0300

    Start to built calculator

---
+++ variables/Ch5/Claculator.py
@@ -0,0 +1,6 @@
+'''Calculator: Build a calculator that performs simple arithmetic operations such as addition, subtraction, multiplication, and division using variables to store the values.'''
+def sum_cal(*arg):
+    for i in arg:
+        return sum(arg)
+
+

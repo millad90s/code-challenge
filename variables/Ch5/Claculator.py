9ad7c726f9bbcedc056fe14b89c745a130ceffbf
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-11 00:02:49 +0300

    Remove for

--- variables/Ch5/Claculator.py
+++ variables/Ch5/Claculator.py
@@ -1,6 +1,5 @@
 '''Calculator: Build a calculator that performs simple arithmetic operations such as addition, subtraction, multiplication, and division using variables to store the values.'''
 def sum_cal(*arg):
-    for i in arg:
         return sum(arg)
 
 

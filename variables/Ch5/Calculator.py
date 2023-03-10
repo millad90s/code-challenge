a04544e582c3547a07fa0787e7906eb8fb401a9a
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-11 01:58:49 +0300

    rename file

---
+++ variables/Ch5/Calculator.py
@@ -0,0 +1,9 @@
+'''Calculator: Build a calculator that performs 
+simple arithmetic operations such as addition, 
+subtraction, multiplication, and division using 
+variables to store the values.'''
+def sum_cal(*args):
+    return sum(args)
+
+
+

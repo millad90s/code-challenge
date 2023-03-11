6cc29b8f98226649f123da5374055d1b1c051d3a
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-11 19:00:37 +0300

    complete sum multi div subtraction

--- variables/Ch5/Calculator1.py
+++ variables/Ch5/Calculator1.py
@@ -5,11 +5,29 @@ Created on Sat Mar 11 01:28:01 2023
 @author: user1
 """
 
-def sum_cal(*args):
+def sums(*args):
     return sum(args)
 
-def multiplication(*args):
-    result=1
+def subtraction(*args):
+    result = args[0]
+    for i in args[1:]:
+        result+=-i
+    return result
+
+def multis(*args):
+    result = 1
     for i in args:
         result*=i
     return result
+
+def divs(*args):
+    result = args[0]
+    for i in args[1:]:
+        result*=(1/i)
+    return result
+
+
+
+    
+
+    

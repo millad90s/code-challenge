84cb25384ad4926c8505cc81f87da7726982e4b1
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-13 17:07:18 +0300

    create average function with * Arg

---
+++ variables/ch6/Average.-Number-and-notebook.py
@@ -0,0 +1,6 @@
+'''Gradebook: 
+    Create a gradebook for a class using variables to store the students' grades 
+    and calculate the average grade for the class.'''
+def avg_num(*num):
+    return sum(num)/len(num)
+

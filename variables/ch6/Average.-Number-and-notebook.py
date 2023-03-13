09053c4928c2682b1802f0371c0fdb58abb3cc19
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-14 01:57:06 +0300

    comment insert

--- variables/ch6/Average.-Number-and-notebook.py
+++ variables/ch6/Average.-Number-and-notebook.py
@@ -1,6 +1,8 @@
 '''Gradebook: 
     Create a gradebook for a class using variables to store the students' grades 
     and calculate the average grade for the class.'''
+# Function Average Number 
 def avg_num(*num):
     return sum(num)/len(num)
+
 

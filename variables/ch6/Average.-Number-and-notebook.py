630b605c1165e0b48851b25b1deb74b1c2790392
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-21 15:23:54 +0300

    Set Parameters

--- variables/ch6/Average.-Number-and-notebook.py
+++ variables/ch6/Average.-Number-and-notebook.py
@@ -5,4 +5,8 @@
 def avg_num(*num):
     return sum(num)/len(num)
 
+""" Getting the number of names and the number of lessons  """
+names_student = int(input('How many need do you names for student : '))
+scores_student = int(input('How many need do you score for student : '))
 
+student = {}  # for use dictionary for list

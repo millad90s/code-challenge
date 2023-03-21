73a5081ae0394966ac5c64bddb8e60005a69e851
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-22 01:47:40 +0300

    Create Coments

--- variables/ch7/distance-between-two-points.py
+++ variables/ch7/distance-between-two-points.py
@@ -10,13 +10,13 @@ y1 = input('The width of the first point (y1) = ')
 x2 = input('The length of the seccond point (x2) = ')
 y2 = input('The width of the seccond point (y2) = ')
 
-
+# Check values to numeric values 
 if x1.isnumeric() and y1.isnumeric() and x2.isnumeric() and y2.isnumeric():
-    p = [float(x1), float(y1)]
-    q = [float(x2), float(y2)]
-    print(math.dist(p, q))
+    p = [float(x1), float(y1)] # Define two point from their Coordinates 
+    q = [float(x2), float(y2)] # Define two point from their Coordinates 
+    print(math.dist(p, q))     # Use the dist method to calculate the distance between two points and print them
 else:
-    print('You must insert numeris value')
+    print('You must insert integer value')
 
 
 
